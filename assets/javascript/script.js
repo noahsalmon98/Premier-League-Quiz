c43722ea30383596bd013934f964e9fb508ec01a
Noah Salmon <noahsalmon98@gmail.com>
2023-08-24 14:05:44 -0400

    added questions to javascript file

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -13,3 +13,39 @@
 // THEN the game is over
 // WHEN the game is over
 // THEN I can save my initials and my score
+
+
+//Questions and Answers
+
+var questions = [
+    {
+        prompt: "In what year did the Premier League begin?",
+        possibleAnswers: ["1990","1991","1992","1993",],
+        correctAnswer: "1992",
+    },
+    {
+        prompt: "How many teams are there in the Premier League each season?",
+        possibleAnswers: ["15","20","25","30",],
+        correctAnswer: "20",
+    },
+    {
+        prompt: "Who is the all time leading scorer in the Premier League?",
+        possibleAnswers: ["Harry Kane","Wayne Rooney","Erling Haaland","Alan Shearer",],
+        correctAnswer: "Alan Shearer",
+    },
+    {
+        prompt: "Who has lifted the Premier League trophy the most times in the past decade? (6 times!)",
+        possibleAnswers: ["Manchester City","Liverpool","Manchester United","Chelsea",],
+        correctAnswer: "Manchester City",
+    },
+    {
+        prompt: "How many games are there in a modern Premier League Season?",
+        possibleAnswers: ["36","38","40","42",],
+        correctAnswer: "38",
+    },
+    {
+        prompt: "Who holds the record for most goals in a single Premier League Season?",
+        possibleAnswers: ["Alan Shearer","Harry Kane","Mohamed Salah","Erling Haaland",],
+        correctAnswer: "Erling Haaland",
+    }
+];
